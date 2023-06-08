9929acfd6aea6f7d90d3b440fc941eece47fc7a1
Hadław Michał <michalhadlaw@gmail.com>
2023-06-08 16:23:23 +0200

    thirt commit

--- src/main/java/com/telusko/springbootwebproject/AlienController.java
+++ src/main/java/com/telusko/springbootwebproject/AlienController.java
@@ -2,13 +2,13 @@ package com.telusko.springbootwebproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class AlienController {
     @Autowired
     AlienRepo repo;
@@ -16,27 +16,36 @@ public class AlienController {
     public String home(){
         return "home.jsp";
     }
-@RequestMapping("/addAlien")
-    public String addAlen(Alien alien){
+@PostMapping(value = "/alien")
+    public Alien  addAlien( Alien alien){
         repo.save(alien);
-        return "home.jsp";
+        return alien;
+    }
+    @DeleteMapping("/alien/{aid}")
+    public String  deleteAlien(@PathVariable int aid){
+        Alien a = repo.getOne(aid);
+
+        repo.delete(a);
+
+        return "Deleted";
     }
 
-    @RequestMapping("/aliens")
-    @ResponseBody
-    public String getAlens(){
-       return repo.findAll().toString();
-//        System.out.println(repo.findByTech("Java"));
-//        System.out.println(repo.findByAidGreaterThan(256));
-//        System.out.println(repo.findByTechSorted("Java"));
-//        mv.addObject(alien);
-//        return mv;
+    @GetMapping(path="/aliens",produces = {"application/xml"})
+
+    public List<Alien> getAlens(){
+       return repo.findAll();
+
     }
 
+    @PutMapping(path = "/alien")
+    public Alien  saveUpdateAlien( @RequestBody Alien alien) {
+        repo.save(alien);
+        return alien;
+    }
     @RequestMapping("/alien/{aid}")
-    @ResponseBody
-    public String getAlen(@PathVariable("aid")int aid){
-        return repo.findById(aid).toString();
+
+    public Optional<Alien> getAlen(@PathVariable("aid")int aid){
+        return repo.findById(aid);
 
     }
 
--- src/main/java/com/telusko/springbootwebproject/AlienRepo.java
+++ src/main/java/com/telusko/springbootwebproject/AlienRepo.java
@@ -1,17 +1,14 @@
 package com.telusko.springbootwebproject;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>
+public interface AlienRepo extends JpaRepository<Alien, Integer>
 {
-//List<Alien> findByTech(String tech);
-//List<Alien> findByAidGreaterThan(int aid);
-//
-//@Query("from Alien where tech=?1 order by alienName")
-//List<Alien> findByTechSorted(String tech);
+
 
 
 }
